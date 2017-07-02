87a63110fde24c2748912128267972d854b90e8f
pengjie <pengjie@tigerwit.com>
2017-07-02 23:49:29 +0800

    添加基础样式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import './assets/scss/_index.scss'
 import Vue from 'vue'
 import router from './router'
 import store from './store'
