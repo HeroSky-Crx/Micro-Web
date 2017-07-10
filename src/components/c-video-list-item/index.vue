67a9b955e93577771fba3af2f2b9a5ebce057117
KidneyFlower <753475777@qq.com>
2017-07-10 23:57:34 +0800

    :sparkles: 完成视频挑战列表组件

---
+++ src/components/c-video-list-item/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="c-video-list-item__wrap">
+    <div class="c-video-list-item__left">
+      <img :src="data.img" />
+    </div>
+    <div class="c-video-list-item__right">
+      <span class="c-video-list-item__name">{{data.name}}</span>
+      <div class="c-video-list-item__play"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      data: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import './style.scss';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,25 @@ Vue.use(Router)
 
 import Home from 'views/home'
 
+// 挑战
+import ChallangeLayout from 'views/challange'
+import ChallangeVideo from 'views/challange/video'
+
 export default new Router({
   routes: [
     {
       path: '/',
       component: Home
+    },
+    {
+      path: '/challange',
+      component: ChallangeLayout,
+      children: [
+        {
+          path: 'video',
+          component: ChallangeVideo
+        }
+      ]
     }
   ]
 })
